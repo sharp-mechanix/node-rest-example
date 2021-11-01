fdfa450a2f4aedb207e51c8fc02ecab9db368219
Viktor Pozdnyakov <pozdnyakov@itglobal.ru>
2021-11-01 21:24:03 +0300

    Add CRUD for posts

--- src/contracts/posts.ts
+++ src/contracts/posts.ts
@@ -1,10 +1,10 @@
 /**
  * Posts search query parameters
  */
-export type ListPostsQuery = {
+export type ListPostsQueryDto = {
     title?: string;
     content?: string;
-    createdAt?: string;
+    createdAt?: Date;
 }
 
 /**
@@ -12,8 +12,35 @@ export type ListPostsQuery = {
  */
 export type PostDto = {
     id: number;
-    createdAt: Date,
-    updatedAt: Date,
-    title: string,
-    content?: string
+    createdAt: Date;
+    updatedAt: Date;
+    title: string;
+    content?: string;
+}
+
+/**
+ * Create post request parameters
+ */
+export type CreatePostRequestDto = {
+    title: string;
+    content?: string;
+    authorId: number;
+    published: boolean;
+}
+
+/**
+ * Update post request parameters
+ */
+export type UpdatePostRequestDto = {
+    id: number;
+    title?: string;
+    content?: string;
+    published?: boolean;
+}
+
+/**
+ * Publish post request parameters
+ */
+export type PublishPostRequestDto = {
+    id: number;
 }
--- src/controllers/posts.controller.ts
+++ src/controllers/posts.controller.ts
@@ -1,4 +1,5 @@
-import { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
+import { CreatePostRequestDto, ListPostsQueryDto as ListPostsQueryDto, PostDto, PublishPostRequestDto, UpdatePostRequestDto } from '../contracts/posts';
 
 /**
  * Test controller just to make sure server works
@@ -7,8 +8,94 @@ class PostsController {
     /**
      * Returns test string
      */
-    async getPost(_: Request, response: Response): Promise<void> {
-        response.send("OK");
+    async getPosts(request: ListPostsQueryDto): Promise<PostDto[]> {
+        const prisma = new PrismaClient();
+
+        const posts = await prisma.post.findMany({
+            where: {
+                title: request.title,
+                content: request.content,
+                createdAt: request.createdAt
+            }
+        });
+
+        return posts;
+    }
+
+    /**
+     * Gets post by ID
+     */
+    async getPostById(id: number): Promise<PostDto> {
+        const prisma = new PrismaClient();
+
+        const post = await prisma.post.findFirst({ where: { id } });
+
+        return post;
+    }
+
+    /**
+     * Creates new post
+     */
+    async createPost(request: CreatePostRequestDto): Promise<PostDto> {
+        const prisma = new PrismaClient();
+
+        const newPost = await prisma.post.create({
+            data: {
+                title: request.title,
+                content: request.content,
+                published: request.published,
+                authorId: request.authorId
+            }
+        });
+
+        return newPost;
+    }
+
+    /**
+     * Creates new post
+     */
+     async updatePost(request: UpdatePostRequestDto): Promise<PostDto> {
+        const prisma = new PrismaClient();
+
+        const updatedPost = await prisma.post.update({
+            where: {
+                id: request.id
+            },
+            data: {
+                title: request.title,
+                content: request.content,
+                published: request.published
+            }
+        });
+
+        return updatedPost;
+    }
+
+    /**
+     * Mark post as published
+     */
+    async publishPost(id: number): Promise<PostDto> {
+        const prisma = new PrismaClient();
+
+        const publishedPost = await prisma.post.update({
+            where: {
+                id
+            },
+            data: {
+                published: true
+            }
+        });
+
+        return publishedPost;
+    }
+
+    /**
+     * Delete post by ID
+     */
+    async deletePost(id: number): Promise<void> {
+        const prisma = new PrismaClient();
+
+        await prisma.post.delete({ where: { id }});
     }
 }
 
